1aa2504304de65b2ae832fcb1ea3b86bd511eb22
biacamargg <anabeatrizcamargo019@gmail.com>
2026-09-15 18:29:09 -0300

    Create script.js
    Adicionar funcionalidades da calculadora

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        const valor = botao.textContent;
+
+        if (valor === "C") {
+            display.value = "";
+        } 
+        else if (valor === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Erro";
+            }
+        } 
+        else {
+            display.value += valor;
+        }
+    });
+});
